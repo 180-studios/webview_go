4e94858bda363c2e1fc90128d499e79fd0443811
Josh GM Walker <56300765+Josh-Walker-GM@users.noreply.github.com>
2025-09-04 04:26:18 +0100

    begin minimal example

---
+++ examples/uri-scheme/main.go
@@ -0,0 +1,23 @@
+package main
+
+import webview "github.com/180-studios/webview_go"
+
+func main() {
+	w := webview.New(false)
+	defer w.Destroy()
+	w.SetTitle("Basic Example")
+	w.SetSize(480, 320, webview.HintNone)
+
+	w.RegisterURIScheme("app.local", handleURIScheme)
+	w.Navigate("app.local://index.html")
+
+	w.Run()
+}
+
+func handleURIScheme(uri string, path string) (webview.URISchemeResponse, error) {
+	return webview.URISchemeResponse{
+		Status:      200,
+		ContentType: "text/html",
+		Data:        []byte("<h1>Hello from " + path + "</h1>"),
+	}, nil
+}
